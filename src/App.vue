41d1623d80880c0aaaeaefd4e734391768ab4381
jack755051 <jack755051@gmail.com>
2024-10-29 01:51:08 +0800

    first commit lesson 15

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<template>
+    <Person></Person>
+    <br/>
+    <Person02></Person02>
+</template>
+
+<script lang="ts">
+    import Person from "./components/Person.vue"
+import Person02 from "./components/Person02..vue";
+
+    export default {
+        name:"App",
+        components:{Person,Person02}
+    }
+</script>
+
+<style>
+    
+</style>
