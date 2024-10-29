0eb9a7a2be27acaf4985a1bf62f8717f1266492a
charlieTai <charlie.tai@tpisoftware.com>
2024-10-29 13:44:37 +0800

    modify：變更設定檔config

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,15 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import { defineConfig } from 'vite';
+import vue from '@vitejs/plugin-vue';
+import { fileURLToPath, URL } from 'node:url';
+import VueSetupExtend from 'vite-plugin-vue-setup-extend';
+
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [vue(), VueSetupExtend()],
+  resolve:{
+    alias:{
+      '@':fileURLToPath(new URL('./src', import.meta.url))
+    }
+  }
 })
