095ad3e5e8729f2f301eb89aa7853c0dc50446c7
charlieTai <charlie.tai@tpisoftware.com>
2024-10-29 14:33:56 +0800

    add:建立vue模板

---
+++ src/components/Template.vue
@@ -0,0 +1,24 @@
+<template>
+    <div class="template">
+        
+    </div>
+</template>
+
+<script lang="ts">
+export default {
+    name:"template",
+}
+</script>
+
+<script lang="ts" setup>
+    
+</script>
+
+<style>
+    .template{
+        background-color: #ddd;
+        box-shadow: 0 0 10px;
+        border-radius: 10px;
+        padding: 20px;
+    }
+</style>
